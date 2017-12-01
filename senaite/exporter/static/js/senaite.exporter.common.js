5f36073cc393c648fa2de16828c853b6fc0be957
Espurna <psoliva@naralabs.com>
2017-12-01 12:28:52 +0100

    function description

--- senaite/exporter/static/js/senaite.exporter.common.js
+++ senaite/exporter/static/js/senaite.exporter.common.js
@@ -7,6 +7,11 @@ window.onload = function() {
 };
 
 function export_submit_controller() {
+    /**
+    This function submits the form once the filter button is clicked.
+    The function sets the input[name="export-list-submission"]
+    value as '1'.
+    */
     $('input#export-list-submission-fake').bind('click', function () {
             var form = $(this).closest("form");
             $('input#export-list-submission').val('1');
